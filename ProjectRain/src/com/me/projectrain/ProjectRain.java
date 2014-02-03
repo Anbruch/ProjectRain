2f951ff2834749fc4d83ff60aa585d338263e676
Madslick <joeyauby@gmail.com>
2014-02-03 14:05:52 -0800

    Back end code, Got an image on the screen :)

--- ProjectRain/src/com/me/projectrain/ProjectRain.java
+++ ProjectRain/src/com/me/projectrain/ProjectRain.java
@@ -1,55 +1,49 @@
 package com.me.projectrain;
 
+import projectrain.tools.Assets;
+import projectrain.tools.WorldController;
+import projectrain.tools.WorldRenderer;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class ProjectRain implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+	boolean paused;//Game being paused handled in main
+	
+	//Variables for the world
+	
+	
+	
 	
 	@Override
-	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+	public void create() {	
 		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		Assets.instance.init(new AssetManager());//Make the Spritesheet to be cut from later
+		WorldController.controller.init();
+		WorldRenderer.renderer.init();
+		paused = false;
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		texture.dispose();
+		
 	}
 
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		if(!paused){
+			WorldController.controller.update(Gdx.graphics.getDeltaTime());
+		}
+		
+		
+		Gdx.gl.glClearColor(1, 1, 1, 0); //Default background color
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		WorldRenderer.renderer.render();
+
+		
 	}
 
 	@Override
